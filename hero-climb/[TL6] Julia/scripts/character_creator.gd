56e47fc35eb0953bd6aa401d87f3ea9fcda62ee9
Julia <themusicalartist12@gmail.com>
2024-09-22 16:05:34 -0700

    slight change

--- hero-climb/[TL6] Julia/scripts/character_creator.gd
+++ hero-climb/[TL6] Julia/scripts/character_creator.gd
@@ -1,8 +1,19 @@
 extends Control
 
-@onready var WizardButton: Button = $VFlowContainer/GridContainer/Wizard
-@onready var FigherButton: Button = $VFlowContainer/GridContainer/Fighter
-@onready var RogueButton: Button = $VFlowContainer/GridContainer/Rogue
+@onready var WizardButton: Button = $HFlowContainer/GridContainer/Wizard
+@onready var FigherButton: Button = $HFlowContainer/GridContainer/Fighter
+@onready var RogueButton: Button = $HFlowContainer/GridContainer/Rogue
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
 
 
 func _on_rogue_pressed() -> void:
