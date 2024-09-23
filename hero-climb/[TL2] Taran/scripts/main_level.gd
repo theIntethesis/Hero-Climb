2762e80730dd83a90c677c55190f796ff6c51de2
Tmanatee <taranhaug@gmail.com>
2024-09-23 15:21:20 -0700

    Set up very basic level generation

---
+++ hero-climb/[TL2] Taran/scripts/main_level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const FLOOR_HEIGHT = 223
+
+func _ready():
+	var floors = [preload("res://[TL2] Taran/scenes/tower pieces/tp1.tscn"),
+		preload("res://[TL2] Taran/scenes/tower pieces/tp2.tscn")]
+	
+	var level = 1
+	while( level < 5):
+		var new_floor = floors[randi()%2].instantiate()
+		add_child(new_floor)
+		new_floor.global_position.y -= level*FLOOR_HEIGHT
+		level += 1
+	
