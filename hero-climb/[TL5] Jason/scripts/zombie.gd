b091d1e7d813ee6eaac4eddd76e8c29da1ee5557
Jason C. <jtculbertson@hotmail.com>
2024-09-30 17:23:54 -0700

    Updated Zombie

--- hero-climb/[TL5] Jason/scripts/zombie.gd
+++ hero-climb/[TL5] Jason/scripts/zombie.gd
@@ -1,27 +1,52 @@
 extends CharacterBody2D
+class_name Zombie
 
 # Variables to control speed and direction
-@export var speed = 100
-var direction = Vector2.RIGHT
+@export var speed = 50
+var direction = Vector2.LEFT
+var health = 100
+var damage = 25
 
-# Define the boundaries for movement
-@export var left_limit = -200
-@export var right_limit = 200
+var playerDetected = false
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	# Move the zombie
+	velocity = direction * speed
+	
+	# Gravity
+	if !is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Flipping
+	$AnimatedSprite2D.flip_h = true if velocity.x < 0 else false
+	
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.stop()
+
 
 func _ready():
-	# Set the initial position
-	position.x = left_limit
-	# Start the run animation
-	$AnimatedSprite2D.play("run")
+	pass
+	
 
 func _process(delta):
-	# Move the zombie
-	position += direction * speed * delta
+	
+	if $"Detect Player".overlaps_body(%Player):
+		$AnimatedSprite2D.play("run")
+		var dir = position.x - %Player.position.x
+		direction = Vector2.RIGHT if dir < 0 else Vector2.LEFT
+	else:
+		direction = Vector2.ZERO
 
-	# Check if the zombie has reached the limits
-	if position.x >= right_limit:
-		direction = Vector2.LEFT
-		$AnimatedSprite2D.flip_h = true  # Flip the sprite horizontally
-	elif position.x <= left_limit:
-		direction = Vector2.RIGHT
-		$AnimatedSprite2D.flip_h = false  # Reset the flip
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "Fireball":
+		health -= 20
+		print(health)
+	if health == 0:
+		queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body == %Player:
+		%Player.Health -= damage
+		print(%Player.Health)
