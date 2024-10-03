cc18ecd8ad8053e40b3b15a17eec233825cb8d11
Tmanatee <taranhaug@gmail.com>
2024-10-03 10:09:42 -0700

    Built some level pieces

--- hero-climb/[TL2] Taran/scripts/base_level.gd
+++ hero-climb/[TL2] Taran/scripts/base_level.gd
@@ -2,11 +2,13 @@ extends Node2D
 class_name Level
 
 const PIECE_COUNTS = {"LY":0, "LC":0, "LN":0,
-					"CY":0, "CC":0, "CN":0,
+					"CY":2, "CC":2, "CN":2,
 					"RY":0, "RC":0, "RN":0,}
 
+# L is left, C is center, R is right
 const LAYOUT_MAP = ["L","C","C","R"]
 
+# Y is a route up, C is a class route, N is no route, S is a shops
 var level_layout = ["Y","C","N","S"]
 
 func _ready():
@@ -22,6 +24,6 @@ func build_level():
 		else:
 			print("res://[TL2] Taran/scenes/tower pieces/"
 				+LAYOUT_MAP[level_place]+level_layout[level_place]
-				+str(randi_range(0,PIECE_COUNTS[LAYOUT_MAP[level_place]+level_layout[level_place]]))
+				+str(randi_range(1,PIECE_COUNTS[LAYOUT_MAP[level_place]+level_layout[level_place]]))
 				+".tscn")
 		level_place += 1
