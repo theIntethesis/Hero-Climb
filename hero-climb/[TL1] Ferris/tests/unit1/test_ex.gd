08b46bf10f90d747f46bdd69f2d996ea82816d6c
theIntethesis <hayden@gnorth.net>
2024-10-15 08:00:31 -0700

    Updated Ferris's Test

--- hero-climb/[TL1] Ferris/tests/unit1/test_ex.gd
+++ hero-climb/[TL1] Ferris/tests/unit1/test_ex.gd
@@ -1,5 +1,27 @@
 extends GutTest
 
+
+func test_input():
+	var Scene = load("res://[TL1] Ferris/scenes/PlayerController.tscn")
+	var FloorScene = load("res://[TL1] Ferris/scenes/floor.tscn")
+	var instance = Scene.instantiate()
+	var floorInstance = FloorScene.instantiate()
+	add_child(instance)
+	add_child(floorInstance)
+	var Player = get_node("Player")
+	var cam = Camera2D.new()
+	cam.offset = Vector2(30, 0)
+	Player.add_child(cam)
+	
+	wait_seconds(2)
+	var Sender = InputSender.new(Player).wait(1)
+	
+	Sender.action_down("jump")
+	print(Player.get_velocity())
+	await(Sender.idle)
+	assert_true(Player.get_velocity() < Vector2.ZERO)
+	
+
 func test_ex():
 	assert_eq(1, 1)
 	
