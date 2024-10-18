5235698d38bfe0b320d099a20a95da9b437733a8
Gavin <gavinhaynes32@gmail.com>
2024-10-18 15:30:59 -0700

    Continued working on player sound controller

--- hero-climb/[TL3] Gavin/scripts/player_sound_player.gd
+++ hero-climb/[TL3] Gavin/scripts/player_sound_player.gd
@@ -1,17 +1,68 @@
+# player_sound_controller.gd
+# Gavin Haynes
+# Began October 17, 2024
+
+# Description
+# Play and manage player sounds. 
+
 extends Node2D
 
-var childList
+var _volume: int
 
-# enum {ATTACK, JUMP, WALK, DAMAGE}
+# Play a sound
+func play(soundName: String) -> bool:
+	match soundName:
+		"attack":
+			$Attack.play()
+			return true;
+		"jump":
+			$Jump.play()
+			return true;
+		"damaged":
+			$PlayerDamaged.play()
+			return true;
+		"walk":
+			$Walking.play()
+			return true;
+		_:
+			return false;
+
+# Add or subtract from the current volume by delta
+func changeVolume(delta: int) -> bool:
+	var result = _volume + delta
+	if (result >= 0 && result <= 100):
+		_volume = result
+		return true;
+	else:
+		return false;
+
+# Set the volume to a value int [0-100]
+func setVolume(volume: int) -> bool:
+	if (volume >= 0 && volume <= 100):
+		_volume = volume
+		return true
+	else:
+		return false
+
+# Return the volume
+func getVolume() -> int:
+	return _volume
+
+# Convert a value from 0-1 to a db value
+func volumeToDb(volume: int) -> int:
+	return linear_to_db(volume/100.0)
+
+func checkVolume(volume: int) -> bool:
+	return false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	childList = get_children()
+	setVolume(80)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-# Play a 
-func play(sound):
-	childList[sound].play()
+func _set_child_volume(volume: int) -> void:
+	for child in get_children():
+		child.volume_db = volumeToDb(volume)
