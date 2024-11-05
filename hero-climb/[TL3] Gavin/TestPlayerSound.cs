9a1547965688092c4b539f49c9bd8866845f005a
Gavin <gavinhaynes32@gmail.com>
2024-11-04 16:15:52 -0800

    Update TestPlayerSound.cs

--- hero-climb/[TL3] Gavin/TestPlayerSound.cs
+++ hero-climb/[TL3] Gavin/TestPlayerSound.cs
@@ -5,6 +5,24 @@ public static class TestPlayerSound
     [CSTestFunction]
     public static Result SetVolume1()
     {
-        
+        return Result.Success;
+    }
+
+    [CSTestFunction]
+    public static Result SetVolume2()
+    {
+        return Result.Success;
+    }
+
+    [CSTestFunction]
+    public static Result SetVolume3()
+    {
+        return Result.Success;
+    }
+
+    [CSTestFunction]
+    public static Result SetVolume4()
+    {
+        return Result.Success;
     }
 }
