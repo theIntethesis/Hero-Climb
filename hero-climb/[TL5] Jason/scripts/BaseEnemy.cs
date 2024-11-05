4b835b78907b80c5209b327c522203f72e3ae362
JTCulbertson <jtculbertson@hotmail.com>
2024-11-04 23:21:25 -0800

    New Enemies

--- hero-climb/[TL5] Jason/scripts/BaseEnemy.cs
+++ hero-climb/[TL5] Jason/scripts/BaseEnemy.cs
@@ -3,24 +3,31 @@ using System;
 
 public abstract partial class BaseEnemy : CharacterBody2D
 {
+	#region FIELDS
 	[Export] public int Damage = 20;
-	public float Gravity = 10.0f;
+	public float Gravity = 100.0f;
 	public float Speed = 50.0f;
 	public int Health = 100;
 	private Vector2 direction = new Vector2(1, 0);  // Initial direction: right
 	private AnimatedSprite2D sprites;  // Reference to the sprite node
 	private Timer turnTimer;  // Timer for handling cooldown between direction changes
-	private Node player;
+	private CharacterBody2D player;
 	private bool IsDetectingPlayer = false;
 	private Vector2 playerPosition;
+	private bool IsDead = false;
+	private bool IsIdle = false;
 
+	#endregion
+	
+	#region SETUP
 	public override void _Ready()
 	{
 		GD.Print("BaseEnemy ready.");
 
-
+		player =  (CharacterBody2D)GetParent().GetParent().GetNode("Player");
 		// Get the sprite node
 		sprites = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		sprites.Play("play");
 
 		// Initialize the timer
 		turnTimer = new Timer();
@@ -33,25 +40,32 @@ public abstract partial class BaseEnemy : CharacterBody2D
 	{
 		GD.Print("BaseEnemy setup.");
 	}
+	#endregion
 
-//	public override void _Process(double delta)
-//	{
-//		if (IsDetectingPlayer){
-//			if (GlobalPosition.X - playerPosition.X < 0){
-//				direction = new Vector2(1,0);
-//				FlipSprite();
-//				GD.Print("Walking right towards player");
-//			} else {
-//				direction = new Vector2(-1,0);
-//				FlipSprite();
-//				GD.Print("Walking left towards player");
-//			}
-//		} else {
-//			GD.Print("Not Detecting Player");
-//		}
-//	}
+	public override void _Process(double delta)
+	{
+	}
+
+
 	public override void _PhysicsProcess(double delta)
 	{
+
+
+		if (Math.Floor(GlobalPosition.X) == Math.Floor(player.GlobalPosition.X)){
+			Velocity = new Vector2(0,0);
+			IsIdle = true;
+		}
+
+		if (IsDetectingPlayer && !IsDead && !IsIdle){
+			if (GlobalPosition.X - player.GlobalPosition.X < 0){
+				direction = new Vector2(1,0);
+				sprites.Scale = new Vector2(-1,1);
+			} else {
+				direction = new Vector2(-1,0);
+				sprites.Scale = new Vector2(1,1);
+			}
+		}
+
 		Vector2 velocity = Velocity;
 
 		// Apply gravity
@@ -60,9 +74,8 @@ public abstract partial class BaseEnemy : CharacterBody2D
 			velocity.Y += Gravity * (float)delta;
 		}
 
-
 		// Move the enemy back and forth
-		velocity.X = direction.X * Speed;
+			velocity.X = direction.X * Speed;
 
 		// Change direction on wall/ledge collision
 		if (IsOnWall() || !IsOnFloor())
@@ -77,17 +90,34 @@ public abstract partial class BaseEnemy : CharacterBody2D
 		}
 
 		// Move the enemy
-		Velocity = velocity;
+		if (IsDead || IsIdle){
+			Velocity= new Vector2(0,0);
+		} else {
+			Velocity = velocity;
+		}
+		
 		MoveAndSlide();
 	}
 	
+	#region METHODS
 	private void OnArea2DBodyEntered(Node2D body)
 	{
-		if (body is Controller){
-			GD.Print("Yipee");
+		if (body is Controller && !IsDead){
+			EnemyAttack();
 		}
-		if (body is Fireball){
-			GD.Print("Ouch");
+	}
+
+	private void OnArea2DEntered(Area2D area)
+	{
+		if (area is Attack){
+			var attack = (Attack)area;
+			sprites.Play("damage");
+			Health = Health - attack.Damage;
+			GD.Print("Health: " + Health);
+		}
+		if (Health <= 0){
+			Die();
+			GD.Print("Dying now...");
 		}
 	}
 
@@ -95,8 +125,6 @@ public abstract partial class BaseEnemy : CharacterBody2D
 	{
 		if (body is Controller){
 			IsDetectingPlayer = true;
-			playerPosition = body.GlobalPosition;
-			GD.Print("Player Detected");
 		}
 	}
 
@@ -104,7 +132,6 @@ public abstract partial class BaseEnemy : CharacterBody2D
 	{
 		if (body is Controller){
 			IsDetectingPlayer = false;
-			GD.Print("Player Exited Range");
 		}
 	}
 
@@ -113,7 +140,30 @@ public abstract partial class BaseEnemy : CharacterBody2D
 		sprites.FlipH = !sprites.FlipH;
 	}
 
-	public virtual void Attack()
+	public virtual void EnemyAttack()
 	{
+		sprites.Play("attack");
 	}
-}
+
+	public void OnAnimationFinished()
+	{
+		if (IsDead){
+			QueueFree();
+		} else if (IsIdle) {
+			sprites.Play("stand");
+			IsIdle = false;
+		} 
+		else {
+			sprites.Play("play");
+		}
+		
+	}
+
+	private async void Die()
+	{
+		sprites.Play("die");
+		GD.Print("Death animation playing");
+		IsDead = true;
+	}
+	#endregion
+}	
--- hero-climb/[TL5] Jason/scripts/Slime.cs
+++ hero-climb/[TL5] Jason/scripts/Slime.cs
@@ -1,26 +1,26 @@
 using Godot;
 using System;
 
-public partial class Zombie : BaseEnemy
+public partial class Slime : BaseEnemy
 {
 	public override void SetupEnemy()
 	{
 		base.Damage = 25;
 		base.Health = 100;
-		base.Speed = 25;
-		GD.Print("Zombie setup complete.");
+		base.Speed = 20;
+		GD.Print("Slime setup complete.");
 
 	}
 
 	public override void EnemyAttack()
 	{
 		base.EnemyAttack();
-		GD.Print("Zombie attacks!");
+		GD.Print("Slime attacks!");
 	}
 
 	public override void _Ready()
 	{
-		GD.Print("Zombie ready.");
+		GD.Print("Slime ready.");
 		base._Ready();
 	}
 }
