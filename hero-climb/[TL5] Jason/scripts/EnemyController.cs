2664a4097ef82b9cea988ed47c6cd6b27289143e
Tmanatee <taranhaug@gmail.com>
2024-11-23 16:14:09 -0800

    Integrated Monster Params

--- hero-climb/[TL5] Jason/scripts/EnemyController.cs
+++ hero-climb/[TL5] Jason/scripts/EnemyController.cs
@@ -30,10 +30,11 @@ public partial class EnemyController : Node2D
 		int enemyType;
 		foreach (var spawnPoint in spawns)
 		{
-			// Taran Pair Programming
+			// Taran Pair Programming -
 			uint RandomInt = GD.Randi()%100;
 			int SpawnChance = GameDifficultyHandler.Instance().LevelParams().EnemyChance;
 			if( RandomInt >= SpawnChance ){continue;}
+			// -
 
 			enemyType = rand.Next(4);
 			switch (enemyType)
@@ -41,8 +42,9 @@ public partial class EnemyController : Node2D
 				case 0:
 					BaseEnemy enemy0 = (Zombie)ZombieScene.Instantiate();
 					enemy0.GlobalPosition = spawnPoint;
-					enemy0.Damage = damage;
-					enemy0.SetMaxHealth(health);
+					enemy0.Damage = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Zombie).BaseDamage;
+					enemy0.SetMaxHealth(GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Zombie).BaseMaxHealth);
+					enemy0.Speed = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Zombie).BaseSpeed;
 					AddChild(enemy0);
 					enemy0.SetupEnemy();  // Custom setup method for enemies
 					enemy0.SetScale();
@@ -50,24 +52,27 @@ public partial class EnemyController : Node2D
 				case 1:
 					BaseEnemy enemy1 = (Skeleton)SkeletonScene.Instantiate();
 					enemy1.GlobalPosition = spawnPoint;
-					enemy1.Damage = damage;
-					enemy1.SetMaxHealth(health);
+					enemy1.Damage = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Skeleton).BaseDamage;
+					enemy1.SetMaxHealth(GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Skeleton).BaseMaxHealth);
+					enemy1.Speed = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Skeleton).BaseSpeed;
 					AddChild(enemy1);
 					enemy1.SetupEnemy();  // Custom setup method for enemies
 					break;
 				case 2:
 					BaseEnemy enemy2 = (Slime)SlimeScene.Instantiate();
 					enemy2.GlobalPosition = spawnPoint;
-					enemy2.Damage = damage;
-					enemy2.SetMaxHealth(health);
+					enemy2.Damage = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Slime).BaseDamage;
+					enemy2.SetMaxHealth(GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Slime).BaseMaxHealth);
+					enemy2.Speed = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Slime).BaseSpeed;
 					AddChild(enemy2);
 					enemy2.SetupEnemy();  // Custom setup method for enemies
 					break;
 				case 3:
 					BaseEnemy enemy3 = (Goblin)GoblinScene.Instantiate();
 					enemy3.GlobalPosition = spawnPoint;
-					enemy3.Damage = damage;
-					enemy3.SetMaxHealth(health);
+					enemy3.Damage = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Goblin).BaseDamage;
+					enemy3.SetMaxHealth(GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Goblin).BaseMaxHealth);
+					enemy3.Speed = GameDifficultyHandler.Instance().MonsterParams(MonsterTypes.Goblin).BaseSpeed;
 					AddChild(enemy3);
 					enemy3.SetupEnemy();  // Custom setup method for enemies
 					break;
